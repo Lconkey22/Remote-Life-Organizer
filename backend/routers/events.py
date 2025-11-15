02645fe5772433133e5a3cd7b30ac2dfa281fa03
clawton2-Maryville <clawton2@live.maryville.edu>
2025-11-14 18:41:24 -0600

    Create events.py

---
+++ backend/routers/events.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+
+router = APIRouter(prefix="/events", tags=["Events"])
+
+fake_events = [
+    {"id": 1, "name": "Zoom meeting with professor", "date": "2025-02-10"},
+    {"id": 2, "name": "Work shift", "date": "2025-02-12"}
+]
+
+@router.get("/")
+def get_events():
+    return fake_events
