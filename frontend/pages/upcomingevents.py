a6dfa2d4063c625445d6a5fa166718a2b37d2db2
clawton2-Maryville <clawton2@live.maryville.edu>
2025-11-30 13:01:54 -0600

    Update upcomingevents.py

--- frontend/pages/upcomingevents.py
+++ frontend/pages/upcomingevents.py
@@ -1,1 +1,9 @@
+from nicegui import ui
+from frontend.theme import apply_global_theme
+from frontend.pages.home import add_shared_menu 
 
+
+@ui.page('/upcomingevents')
+def upcomingevents_page():
+    apply_global_theme()
+    add_shared_menu()
