8a444873142f0355558391e286a7ae15eb40eef7
clawton2-Maryville <clawton2@live.maryville.edu>
2025-11-30 13:01:14 -0600

    Update timetracker.py

--- frontend/pages/timetracker.py
+++ frontend/pages/timetracker.py
@@ -1,1 +1,9 @@
+from nicegui import ui
+from frontend.theme import apply_global_theme
+from frontend.pages.home import add_shared_menu 
 
+
+@ui.page('/timetracker')
+def timetracker_page():
+    apply_global_theme()
+    add_shared_menu()
