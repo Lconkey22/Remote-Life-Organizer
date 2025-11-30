69465683ec53f01f1bb23222784dbea1ee6b0bc6
clawton2-Maryville <clawton2@live.maryville.edu>
2025-11-30 11:16:56 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from nicegui import ui
 
+from frontend.theme import apply_global_theme
+
+# apply global styling ONE TIME
+apply_global_theme()
+
+
 # Import your page modules so their @ui.page decorators run
 import frontend.pages.events
 import frontend.pages.home
