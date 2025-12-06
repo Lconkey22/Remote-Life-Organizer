2eeb4df97d7cf3a1c73924d891898944a2f17e7a
clawton2-Maryville <clawton2@live.maryville.edu>
2025-12-05 21:52:51 -0600

    Update profile.py

--- frontend/pages/profile.py
+++ frontend/pages/profile.py
@@ -1,6 +1,6 @@
 from nicegui import ui
 from frontend.theme import apply_global_theme
-from frontend.add_shared_menu import add_shared_menu 
+from frontend.menutheme import add_shared_menu 
 
 # -------------------------
 # PROFILE PAGE  (/profile)
