9cb392edc38d70f7f5bae9b45fb0970483d1bd23
clawton2-Maryville <clawton2@live.maryville.edu>
2025-12-05 20:57:59 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import frontend.pages.home
 import frontend.pages.login
 import frontend.pages.profile 
 import frontend.pages.upcominghomework
+import frontend.pages.upcomingevents
+import frontend.pages.timetracker
 
 # Redirect root to login
 @ui.page('/')
